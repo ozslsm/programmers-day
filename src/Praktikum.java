2d152f31629e27f7ed2f93787aef1a20c523c692
Alexey Morozov <amloer@yandex.ru>
2023-01-15 21:17:53 +0100

    Init

---
+++ src/Praktikum.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Praktikum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.println("Это високосный год.");
+        } else {
+            System.out.println("Это не високосный год.");
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else if (year % 4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}

eeff2df55a60314f49ba4b3bd2bd0da15271d2ad
Alexey Morozov <amloer@yandex.ru>
2023-01-15 22:03:17 +0100

    Update conditional

--- src/Praktikum.java
+++ src/Praktikum.java
@@ -15,11 +15,7 @@ public class Praktikum {
     }
 
     public static boolean isLeapYear(int year) {
-        if (year % 400 == 0) {
-            return true;
-        } else if (year % 100 == 0) {
-            return false;
-        } else if (year % 4 == 0) {
+        if (year % 400 == 0 || year % 100 != 0 && year % 4 == 0) {
             return true;
         } else {
             return false;
